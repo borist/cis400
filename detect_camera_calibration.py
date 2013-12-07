d47b6ae3b41af9bd65f6c51b3e9d17aac6a42a8d
Boris Treskunov <borist@seas.upenn.edu>
2013-12-07 12:12:56 -0500

    detecting chessboard distortion working

--- detect_camera_calibration.py
+++ detect_camera_calibration.py
@@ -52,3 +52,19 @@ for fname in images:
         cv2.waitKey()
 
 cv2.destroyAllWindows()
+
+# calibration
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+
+#undistortion
+img = cv2.imread('left12.jpg')
+h,  w = img.shape[:2]
+newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+
+# undistort
+dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+
+# crop the image
+x,y,w,h = roi
+dst = dst[y:y+h, x:x+w]
+cv2.imwrite('calibresult.png',dst)
