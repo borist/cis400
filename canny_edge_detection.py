474e028382f9e5e7574429f445cec0fe13130a43
Boris Treskunov <borist@seas.upenn.edu>
2013-12-10 19:47:27 -0500

    added edge detection file

---
+++ canny_edge_detection.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+import sys
+from matplotlib import pyplot as plt
+
+PATH_TO_IMAGES = 'images/'
+
+
+def canny_edge_detection(filename):
+    img = cv2.imread(filename, 0)
+    edges = cv2.Canny(img, 100, 200)
+
+    plt.subplot(121), plt.imshow(img,cmap = 'gray')
+    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(edges,cmap = 'gray')
+    plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+    plt.show()
+
+if __name__ == "__main__":
+    print "Usage:", sys.argv[0], "image"
+    print sys.argv
+    image_path = sys.argv[1]
+    canny_edge_detection(image_path)
--- detect_camera_calibration.py
+++ detect_camera_calibration.py
@@ -16,7 +16,7 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp[:,:2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
@@ -47,7 +47,7 @@ for fname in images:
 
         try:
             cv2.imshow('img', img)
-            time.sleep(0.5)
+            time.sleep(1)
             cv2.destroyWindow('img')
         except Exception, e:
             exc_type, exc_value, exc_traceback = sys.exc_info()
@@ -59,10 +59,9 @@ for fname in images:
 # Calibrate camera based on test images
 # -------------------------------------
 print "calibrating..."
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 
-#undistortion
 # ----------------------------
 # Undistort the image
 # ----------------------------
