135627512a724f3ce1eda8d4373929052b44bbe0
Boris Treskunov <borist@seas.upenn.edu>
2013-12-11 19:37:15 -0500

    added timing function to camera calibration

--- canny_edge_detection.py
+++ canny_edge_detection.py
@@ -10,7 +10,7 @@ def canny_edge_detection(filename):
     img = cv2.imread(filename, 0)
     edges = cv2.Canny(img, 100, 200)
 
-    plt.subplot(121), plt.imshow(img,cmap = 'gray')
+    plt.subplot(121), plt.imshow(img, cmap = 'gray')
     plt.title('Original Image'), plt.xticks([]), plt.yticks([])
     plt.subplot(122), plt.imshow(edges,cmap = 'gray')
     plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
