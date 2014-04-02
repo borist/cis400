eb0c75f1c680ca6ace36d451874770825bc8055a
Boris Treskunov <borist@seas.upenn.edu>
2014-04-02 01:19:18 -0400

    merge

--- canny_edge_detection.py
+++ canny_edge_detection.py
@@ -7,12 +7,12 @@ PATH_TO_IMAGES = 'images/'
 
 
 def canny_edge_detection(filename):
-    img = cv2.imread(filename, 0)
+    img = cv2.imread(filename, 3)
     edges = cv2.Canny(img, 100, 200)
 
     plt.subplot(121), plt.imshow(img, cmap = 'gray')
     plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122), plt.imshow(edges,cmap = 'gray')
+    plt.subplot(122), plt.imshow(edges, cmap = 'gray')
     plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
     plt.show()
