af5a209b42c0916591c65d47986a6c114f976a9a
Joseph Zhong <jzh@seas.upenn.edu>
2014-04-17 21:31:15 -0400

    fix vp intersect and everything

--- edge.py
+++ edge.py
@@ -1,6 +1,8 @@
 #edge class defines an edge used for vanishing point detection
 
+from __future__ import division
 import sys
+import numpy as np
 
 class Edge:
     def __init__(self, endpoint1, endpoint2):
@@ -20,26 +22,25 @@ class Edge:
         return ((x1+x2)/2, (y1+y2)/2)
 
 # compute a vanishing point hypothesis common to two edges
+def perp(a):
+    b = np.empty_like(a)
+    b[0] = -a[1]
+    b[1] = a[0]
+    return b
+
 def vanishingPoint(edge1, edge2):
-    x1, y1 = edge1.ep1
-    x2, y2 = edge2.ep2
+    a1 = np.array(edge1.ep1)
+    a2 = np.array(edge1.ep2)
+    b1 = np.array(edge2.ep1)
+    b2 = np.array(edge2.ep2)
 
-    x3, y3 = edge2.ep1
-    x4, y4 = edge2.ep2
+    a = a2 - a1
+    b = b2 - b1
+    a_perp = perp(a)
+    denom = np.dot(a_perp, b)
+    if denom == 0:
+        return (sys.maxint, sys.maxint)
+    num = np.dot(a_perp, a1 - b1)
+    result = (num / denom) * b + b1
+    return (int(result[0]), int(result[1]))
 
-    xdiff = x1 - x2, x3 - x4
-    ydiff = y1 - y2, y3 - y4
-
-    def det(a, b):
-        return a[0] * b[1] - a[1] * b[0]
-
-    div = det(xdiff, ydiff)
-    if div == 0:
-        return (-1,-1)
-
-    d = (det(edge1.ep1, edge1.ep2), det(edge2.ep1, edge2.ep2))
-
-    x = det(d, xdiff) / div
-    y = det(d, ydiff) / div
-
-    return x,y
