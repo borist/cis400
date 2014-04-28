e9ab9f2450fbe30f421cdd85c8b2ae852632de3e
Joseph Zhong <jzh@seas.upenn.edu>
2014-04-28 11:02:08 -0400

    resize images prior to running analysis

--- server/colorToPgm.py
+++ server/colorToPgm.py
@@ -4,6 +4,9 @@ import cv2
 def convert(inputfile, outputfile):
     #print outputfile
     img = cv2.imread(inputfile, cv2.CV_LOAD_IMAGE_GRAYSCALE)
+
+    width, height = img.shape[:2]
+    img = cv2.resize(img, (0,0), fx=580/width, fy=580/height)
     #print "writing file %s " % outputfile
     cv2.imwrite(outputfile, img)
     #print "Success!"
